76b6bec53eeba0016f0d4b4666ffbec8bbfdf10b
Ronny Ewanek <ronny.ewanek@gmail.com>
2019-04-02 18:39:27 -0500

    finished auth add dev prod env

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,7 @@
-module.exports = {
-  googleClientID:
-    '1075962556195-2sj9ossv5mueuiqcdij2klqajmq7asuh.apps.googleusercontent.com',
-  googleClientSecret: '-qL5j5AUEwYagprgtjasjXTo',
-  mongoURI:
-    'mongodb+srv://ronny:gattaca101@hooks-wg9na.mongodb.net/test?retryWrites=true',
-  cookieKey: 'wYwc23z7KEw64LV?@&KLs9*'
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./prod')
+} else if (process.env.NODE_ENV === 'ci') {
+  module.exports = require('./ci')
+} else {
+  module.exports = require('./dev')
 }
